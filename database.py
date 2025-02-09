1b302e57150e7bf964c42a151bd4c9d8ca7a2ee0
balram2002 <162151085+balram2002@users.noreply.github.com>
2025-02-09 19:58:03 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 import sqlite3
 from flask import g
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the current directory
+db_path = os.path.join(BASE_DIR, "employee.db") 
+
 def connect_to_Database():
-    sql = sqlite3.connect("C:/Users/asus/Downloads/Projects/Minor Project AAB/employee.db")
+    sql = sqlite3.connect(db_path)
     sql.row_factory = sqlite3.Row 
     return sql
 
